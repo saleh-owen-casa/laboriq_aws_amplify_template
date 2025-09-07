d296bffb1c7f69d334bdada556a75e1eb74b56e5
swadeO <saleh.owen@gmail.com>
2025-09-06 18:58:29 -0600

    updated for errors related to KMS config

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { subscriberPostFunction, subscriberGetFunction } from './api/resource';
 
 defineBackend({
   auth,
   data,
-  subscriberPostFunction,
-  subscriberGetFunction,
 });
